61e453b2927cfa02c7d10d86388c1efbde1b3f16
Leonardo Terrão <leonardo.terrao@tecsinapse.com.br>
2020-04-28 17:19:50 -0300

    Week 3 - Case Study - Introduction to Classification - Homework

---
+++ week-3/case-study-3-introduction-to-classification/homework/draft.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 28 17:09:00 2020
+
+@author: leonardo
+"""
+
+# DO NOT EDIT
+import numpy as np, random, scipy.stats as ss
+
+def majority_vote_fast(votes):
+    mode, count = ss.mstats.mode(votes)
+    return mode
+
+def distance(p1, p2):
+    return np.sqrt(np.sum(np.power(p2 - p1, 2)))
+
+def find_nearest_neighbors(p, points, k=5):
+    distances = np.zeros(points.shape[0])
+    for i in range(len(distances)):
+        distances[i] = distance(p, points[i])
+    ind = np.argsort(distances)
+    return ind[:k]
+
+def knn_predict(p, points, outcomes, k=5):
+    ind = find_nearest_neighbors(p, points, k)
+    return majority_vote_fast(outcomes[ind])[0]
+
